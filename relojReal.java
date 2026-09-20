7ba48c4292e1d490371a5c2361d9447be4c0c178
Skilltreck <alejandroganea223@gmail.com>
2026-09-20 14:21:47 +0200

    Update and rename ejercicio17.java to relojReal.java

--- relojReal.java
+++ relojReal.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class ejercicio17 {
+public class relojReal {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int horas;
